e9d8467b1ef100508f96888bce68496549533fcc
dryabokon <d.ryabokon@gmail.com>
2023-07-21 11:02:58 +0300

    reqs

--- test.py
+++ test.py
@@ -3,10 +3,22 @@ import sys
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    print(sys.argv)
-    # print(' '.join([k for k in os.environ.keys()]))
-    password = os.environ.get("SECRET_LOGIN")
-    if(password=='Password123!'):
-        print('A!!')
-    else:
-        print('((')
+
+    K = [k for k in os.environ.keys()]
+    V = [k for k in os.environ.values()]
+    print(' '.join(K))
+
+    for k,v in zip(K,V):
+        print(k,v)
+
+    # password = os.environ.get("USERNAME")
+    # password = [p for p in password]
+    # print(password)
+
+    # password = os.environ.get("SECRET_LOGIN")
+    #
+    #
+    # if(password=='Password123!'):
+    #     print('A!!')
+    # else:
+    #     print('((')
