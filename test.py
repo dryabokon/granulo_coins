66c09577def830caf765c3aab824ce47da23af95
dryabokon <d.ryabokon@gmail.com>
2023-07-21 10:56:40 +0300

    reqs

--- test.py
+++ test.py
@@ -5,4 +5,8 @@ if __name__ == '__main__':
 
     print(sys.argv)
     # print(' '.join([k for k in os.environ.keys()]))
-    print(os.environ.get("SECRET_LOGIN"))
+    password = os.environ.get("SECRET_LOGIN")
+    if(password=='Password123!'):
+        print('A!!')
+    else:
+        print('((')
